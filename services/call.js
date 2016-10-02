c7801264cd08abbf25e1861ac0d26affdade6078
snollygolly <snollygolly@gmail.com>
2016-10-02 10:43:24 -0500

    added ability to call cat

--- services/call.js
+++ services/call.js
@@ -1,5 +1,25 @@
+const config = require("../data/config.json");
+
+const commandService = require("./command");
+
+const moment = require("moment");
+
 module.exports = {
 	action: (json, sender, rcpt, message) => {
-		return false;
+		return calls["spawn"];
 	}
 };
+
+const calls = {
+	spawn: function spawn(json, sender, rcpt, message, test = false) {
+		const nextCall = moment(json.game.last).add(config.spawn_delay, "minutes").valueOf();
+		const nowCall = moment(new Date()).valueOf();
+		if (nowCall >= nextCall) {
+			// there has been a valid delay
+			return commandService.commands["spawn"](json, sender, rcpt, message, test);
+		}
+		return null;
+	}
+};
+
+module.exports.calls = calls;
