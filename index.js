0f43695301347147bb87d368214595ca75945464
Anthony Michelizzi <cynical89@gmail.com>
2016-10-06 11:39:11 -0700

    moved random kitties out of index

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const config = require("./data/config.json");
 
 const log = require("./helpers/common").log;
 const game = require("./controllers/game");
-// TODO: delete this when randomKitties is moved out of index
+
 const common = require("./helpers/common");
+const randomize = require("./controllers/randomKitties");
 
 const co = require("co");
 const irc = require("irc");
@@ -60,34 +61,17 @@ function onError(err) {
 	console.error(err.stack);
 	throw new Error(err);
 }
-// declare the first random spawn time
-// TODO: move this out of index
-let time = common.getRandomInt(config.min_time, config.max_time);
-// randomly spawn the kitties!
-// TODO: move this out of index
-const randomKitties = () => {
-	co(function* co() {
-		// prepare the next kitty spawn in milliseconds
-		// the current time setting are for min: 8 minutes - max: 30 minutes
-		time = common.getRandomInt(config.min_time, config.max_time);
-		setTimeout(randomKitties, time);
-		// tell the game we want to spawn a kitty!
-		const result = yield game.route(config.irc.nick, config.irc.channels[0], "!spawn");
-		if (result === null) {
-			// nothing should be said
-			return;
-		}
-		// tell the room there is a kitty nearby
-		client.say(config.irc.channels[0], result);
-	}).catch(onError);
-};
-// prepare the first kitty to spawn with a random time
-// TODO: move this out of index
-setTimeout(randomKitties, time);
 
+// prepare the first kitty to spawn randomly
+setTimeout(() => {
+	randomize.randomKitties();
+}, 5000);
+// common.getRandomInt(config.min_time, config.max_time));
 
 process.on("SIGINT", () => {
 	log.info("Kitty is going to sleep...");
 	client.disconnect("Nap time!");
 	process.exit();
 });
+
+module.exports.client = client;
